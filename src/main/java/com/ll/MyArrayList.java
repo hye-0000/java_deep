94c4146cad4e32a8cf265eba31cd27eb1e65503c
hye-0000 <hye_0000@naver.com>
2023-04-19 14:49:09 +0900

    add/get 테스트 통과하도록

--- src/main/java/com/ll/MyArrayList.java
+++ src/main/java/com/ll/MyArrayList.java
@@ -1,21 +1,26 @@
 package com.ll;
 
-public class MyArrayList<E> {
+public class MyArrayList<T> {
     private static final int DEFAULT_CAPACITY = 10; //배열의 기본 용량
     private static final Object[] EMPTY_DATA = {};  //빈 배열
     private int size; //data 배열의 크기를 나타내는 변수
     Object[] data;  //새로운 자료를 담을 배열
 
+
+
     public int size() {
         return size;
     }
 
-    public boolean add(E element) {
+    public boolean add(T element) {
         size++;
         return true;
     }
 
-//    public MyArrayList(){
-//        data = new Object[DEFAULT_CAPACITY]; //디폴트 용량으로 초기화
-//    }
+    public T get(int index) {
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) data[index];
+    }
 }
--- src/test/java/com/ll/Tests.java
+++ src/test/java/com/ll/Tests.java
@@ -26,4 +26,15 @@ public class Tests {
 
         assertThat(list.size()).isEqualTo(2);
     }
+
+    @Test
+    @DisplayName("get(1)")
+    void test03(){
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("포도");
+
+        assertThat(list.get(1)).isEqualTo("포도");
+    }
 }
